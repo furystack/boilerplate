faeeab0a7155f7892a287a36ea9b428b6527284e
Gallay Lajos <gallay.lajos@gmail.com>
2019-10-19 15:00:03 +0200

    feat(frontend):Login and login error in session service

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,16 +3,70 @@ import { SessionService } from "../services/session";
 
 export const Login = Shade({
   shadowDomName: "shade-login",
-  render: ({ injector }) => (
-    <div>
-      Login
-      <button
-        onclick={() => {
-          injector.getInstance(SessionService).login("testuser", "password");
+  initialState: {
+    username: "",
+    password: "",
+    error: ""
+  },
+  construct: ({ injector, updateState }) => {
+    const subscription = injector
+      .getInstance(SessionService)
+      .loginError.subscribe(error => updateState({ error }), true);
+    return () => subscription.dispose();
+  },
+  render: ({ injector, getState, updateState }) => {
+    const { error } = getState();
+    const sessinService = injector.getInstance(SessionService);
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "0 100px"
         }}
       >
-        Login
-      </button>
-    </div>
-  )
+        <div>
+          <h1>Login</h1>
+          <form
+            onsubmit={ev => {
+              ev.preventDefault();
+              const { username, password } = getState();
+              sessinService.login(username, password);
+            }}
+          >
+            <input
+              // value={username}
+              onchange={ev => {
+                updateState(
+                  {
+                    username: (ev.target as HTMLInputElement).value
+                  },
+                  true
+                );
+              }}
+              type="text"
+            />
+            <input
+              // value={password}
+              type="password"
+              onchange={ev => {
+                updateState(
+                  {
+                    password: (ev.target as HTMLInputElement).value
+                  },
+                  true
+                );
+              }}
+            />
+            <button type="submit">Login</button>
+          </form>
+          {error ? <p>{error}</p> : null}
+        </div>
+      </div>
+    );
+  }
 });
--- frontend/src/services/session.ts
+++ frontend/src/services/session.ts
@@ -7,13 +7,15 @@ export type sessionState =
   | "initializing"
   | "offline"
   | "unauthenticated"
-  | "authenticated";
+  | "authenticated"
+  | "loginFailed";
 
 @Injectable({ lifetime: "singleton" })
 export class SessionService {
   public state = new ObservableValue<sessionState>("initializing");
-
   public currentUser = new ObservableValue<User | null>(null);
+
+  public loginError = new ObservableValue("");
   private async init() {
     try {
       const { isAuthenticated } = await this.users.isAuthenticated();
@@ -27,9 +29,13 @@ export class SessionService {
   }
 
   public async login(username: string, password: string) {
-    const usr = await this.users.login({ username, password });
-    this.currentUser.setValue(usr);
-    this.state.setValue("authenticated");
+    try {
+      const usr = await this.users.login({ username, password });
+      this.currentUser.setValue(usr);
+      this.state.setValue("authenticated");
+    } catch (error) {
+      this.loginError.setValue(error.body.message);
+    }
   }
 
   public async logout() {
