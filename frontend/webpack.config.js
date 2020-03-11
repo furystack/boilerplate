8057920ef0437445aca827129cd9ef4fb40ab5bc
Lajos <gallayl@users.noreply.github.com>
2020-03-11 18:11:36 +0100

    Feature/rest (#59)
    * refactor(project): To REST (work-in-progress)
    
    * fix(rest):breaks
    
    * build(vscode): added seed debug profile
    
    * build(webpack):updated default api url
    
    * fix(frontend): error response in session service
    
    * refactor(service): To Rest
    
    * build(package, tsc): fixed services reference, added common to cleanup dirs
    
    * refactor(frontend): replaced awesome-typescript-loader with ts-loader
    
    * ci(azure-pipelines): removed ts-jest, added buildService step

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,102 +1,82 @@
-var path = require("path");
-const webpack = require("webpack");
+var path = require('path')
+const webpack = require('webpack')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TsConfigWebpackPlugin = require("ts-config-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  entry: './src/index.tsx',
   output: {
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].bundle.js",
-    publicPath: "/",
-    path: path.resolve(__dirname + "/bundle")
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname + '/bundle'),
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       cacheGroups: {
         commons: {
           minChunks: 2,
-          name: "vendors",
-          chunks: "all"
+          name: 'vendors',
+          chunks: 'all',
         },
         furystack: {
           test: /([\\/]node_modules[\\/]@furystack[\\/]|[\\/]furystack[\\/]packages[\\/])/gm,
-          name: "furystack",
-          chunks: "all"
-        }
-      }
+          name: 'furystack',
+          chunks: 'all',
+        },
+      },
     },
-    runtimeChunk: false
+    runtimeChunk: false,
   },
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "eval-source-map", // 'source-map',
+  devtool: 'eval-source-map', // 'source-map',
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
   plugins: [
     //new BundleAnalyzerPlugin({ analyzerPort: 8745 }),
-    new TsConfigWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: './index.html',
     }),
     new webpack.EnvironmentPlugin({
-      NODE_ENV: "development",
+      NODE_ENV: 'development',
       DEBUG: true,
-      APP_VERSION: require("./package.json").version,
+      APP_VERSION: require('./package.json').version,
       BUILD_DATE: new Date().toISOString(),
-      SERVICE_URL: "http://localhost:9090"
-    })
+      SERVICE_URL: 'http://localhost:9090/api',
+    }),
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
   ],
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      // { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-        exclude: /monaco-editor/
-      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
       {
         test: /\.css$/,
         use: [
-          require.resolve("style-loader"),
+          require.resolve('style-loader'),
           {
-            loader: require.resolve("css-loader"),
+            loader: require.resolve('css-loader'),
             options: {
-              importLoaders: 1
-            }
-          }
-        ]
+              importLoaders: 1,
+            },
+          },
+        ],
       },
       {
-        test: [
-          /\.bmp$/,
-          /\.gif$/,
-          /\.jpe?g$/,
-          /\.png$/,
-          /\.PNG$/,
-          /\.svg$/,
-          /\.eot$/,
-          /\.woff$/,
-          /\.woff2$/,
-          /\.ttf$/
-        ],
-        loader: require.resolve("url-loader"),
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.PNG$/, /\.svg$/, /\.eot$/, /\.woff$/, /\.woff2$/, /\.ttf$/],
+        loader: require.resolve('url-loader'),
         options: {
           limit: 10000,
-          name: "static/media/[name].[hash:8].[ext]"
-        }
-      }
-    ]
-  }
-};
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+}
