c650304bb52e21fe6c59c5574ebe95c52643d670
Gallay Lajos <gallay.lajos@gmail.com>
2026-02-26 18:44:01 +0100

    dispose fix

--- service/src/root-injector.ts
+++ service/src/root-injector.ts
@@ -1,7 +1,5 @@
 import { Injector } from '@furystack/inject'
 import { useLogging, VerboseConsoleLogger } from '@furystack/logging'
-import { attachShutdownHandler } from './shutdown-handler.js'
 
 export const injector = new Injector()
 useLogging(injector, VerboseConsoleLogger)
-void attachShutdownHandler(injector)
--- service/src/service.ts
+++ service/src/service.ts
@@ -1,7 +1,9 @@
 import { injector } from './root-injector.js'
+import { attachShutdownHandler } from './shutdown-handler.js'
 import { setupStore } from './setup-store.js'
 import { setupRestApi } from './setup-rest-api.js'
 
+void attachShutdownHandler(injector)
 setupStore(injector)
 
 setupRestApi(injector).catch((err) => {
